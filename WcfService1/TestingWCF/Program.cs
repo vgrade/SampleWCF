8296db3082df7663988f34e37fe00bb7f86a8880
vgrade <grade.vishal@gmail.com>
2018-06-28 23:15:40 +0530

    initial

---
+++ WcfService1/TestingWCF/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingWCF.WCFTest;
+namespace TestingWCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatorClient proxy = new CalculatorClient();
+            double a= proxy.AddNumbers(3.2, 0.7);
+        }
+    }
+}
---
+++ WcfService1/WcfService1/ICalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICalculator" in both code and config file together.
+    [ServiceContract]
+    public interface ICalculator
+    {
+        [OperationContract]
+
+        double AddNumbers(double number1, double number2);
+
+        [OperationContract]
+
+        double SubstractNumbers(double number1, double number2);
+
+        [OperationContract]
+
+        double MultiplyNumbers(double number1, double number2);
+
+        [OperationContract]
+
+        double DivisionNumbers(double number1, double number2);
+    }
+}
